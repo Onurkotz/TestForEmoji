7f2de01f0e2d1187b107bfae230aa87a90910b6c
onur koc <onurkottz@gmail.com>
2022-03-19 14:11:52 +0300

    The test file was arranged.

--- src/emoji.test.js
+++ src/emoji.test.js
@@ -1,44 +1,39 @@
 import React from "react";
 import "@testing-library/jest-dom";
-
-
-import {  render, screen } from '@testing-library/react';
+import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 
 import App from "./App";
 
-test("Is header on screen?", () => {
+describe("Tests of App", () => {
+  let input, head, itemDom, letter, smileEmoji;
 
-    render(<App />)
-
-    const head = screen.getByText(/Emoji Search/i);
-    expect(head).toBeInTheDocument();
-});
-
-test("Are 20 items of emojis on list?", () => {
-    render(<App />)
-
-    const itemDom = screen.getAllByText('Click to copy emoji');
-    expect(itemDom.length).toEqual(20);
-})
-
-test("is filter funciton success?", () => {
+  beforeEach(() => {
     render(<App />);
 
-    const input = screen.getByPlaceholderText("Search an emoji!");
-    const letter = "Smile";
-    const smileEmoji = screen.getByText("Smile")
+    input = screen.getByPlaceholderText("Search an emoji!");
+    head = screen.getByText(/Emoji Search/i);
+    itemDom = screen.getAllByText("Click to copy emoji");
+    letter = "Smile";
+    smileEmoji = screen.getByText("Smile");
+  });
+
+  test("Is header on screen?", () => {
+    expect(head).toBeInTheDocument();
+  });
+
+  test("Are there 20 emojis on list?", () => {
+    expect(itemDom.length).toEqual(20);
+  });
+
+  test("Is filter function success?", () => {
     userEvent.type(input, letter);
     expect(smileEmoji).toBeInTheDocument();
-})
+  });
 
-test("is copied?", () => {
-    render(<App />);
-
-    const smileEmoji = screen.getByText("Smile");
+  test("... is copied?", () => {
     userEvent.click(smileEmoji);
-    const input = screen.getByPlaceholderText("Search an emoji!");
     userEvent.paste(input, window.ClipboardItem);
     expect(input).toHaveValue();
-    
-})
+  });
+});
